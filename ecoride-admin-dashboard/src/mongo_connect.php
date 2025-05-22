0cf4e6f562e73fd40a702f000ce31738282f9cfb
Chlomo55 <104308282+Chlomo55@users.noreply.github.com>
2025-05-22 20:51:05 +0200

    Delete ecoride-admin-dashboard directory

--- ecoride-admin-dashboard/src/mongo_connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-// mongo_connect.php
-
-require 'vendor/autoload.php'; // Assurez-vous d'avoir installé le package MongoDB via Composer
-
-$mongoClient = new MongoDB\Client("mongodb://localhost:27017"); // Remplacez par votre URI MongoDB
-?>
